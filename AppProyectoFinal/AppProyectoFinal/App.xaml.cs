1c77e583a5dbc87ed0ef57bf23c34af0e1e25a68
EmmanuelBazan <mqkz99@gmail.com>
2021-06-13 22:59:02 -0500

    Add project files.

---
+++ AppProyectoFinal/AppProyectoFinal/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AppProyectoFinal.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppProyectoFinal
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppProyectoFinal/AppProyectoFinal/Views/Login.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppProyectoFinal.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            var nameValue = txtUsuario.Text;
+            await DisplayAlert("Bienvenido "+ nameValue, "", "OK");
+            Navigation.PushModalAsync(new Menu());
+        }
+    }
+}
